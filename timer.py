dd76e8820932ecbe4ba7a05bdda443ba4ad0313b
Michael Zeevi <michael.52@msn.com>
2019-04-02 13:30:30 +0300

    added best, worst and latest tags to session results.

--- timer.py
+++ timer.py
@@ -4,31 +4,44 @@ import time
 def timeSolve(inspectionTime = 0):
   if inspectionTime:
     inspect(inspectionTime)
-  input('\nPress ENTER to start solve timer...')
+  _ = input('\nPress ENTER to start solve timer... (or `q` to exit)')
   start_time = time.time()
+  if _ == 'q':
+    exit()
   input('Started!\nTiming... Press ENTER to stop!')
   end_time = time.time()
   print('Stopped.\n')
   solveTime = round(end_time - start_time, 4)
   return solveTime
 
-def displaySessionTimes(sessionTimes):
-  if len(sessionTimes):
+def displaySessionSolves(sessionSolves):
+  if len(sessionSolves):
+    times = [solve[1] for solve in sessionSolves]
+    best = min(times)
+    worst = max(times)
+    total = 0
     print('Session results')
     print('---------------')
-    for solveDate, solveTime in sessionTimes.items():
-      print('  {}: {}s'.format(solveDate, solveTime))
-    if len(sessionTimes) > 1:
-      average = round(sum(sessionTimes.values())/len(sessionTimes), 4)
-      best = min(sessionTimes.values())
-      print('Best solve: {}'.format(best))
+    for i, solve in enumerate(sessionSolves):
+      tag = ''
+      if len(sessionSolves) > 1:
+        if solve[1] == best:
+          tag += ' <- Best'
+        if solve[1] == worst:
+          tag += ' <- Worst'
+        if i + 1 == len(sessionSolves):
+          tag += ' <- Latest'
+      print('  {}: {}s{}'.format(solve[0], solve[1], tag))
+      total += solve[1]
+    if len(sessionSolves) > 1:
+      average = round(total/len(times), 4)
       print('Average solve: {}'.format(average))
-      print('---------------------\n')
+      print('------------------\n')
   else:
     print('\nNo solves yet in session. Why not have a go?')
 
 def saveSolveToSession(solve):
-  sessionTimes[time.ctime()] = solve
+  sessionSolves.append((time.ctime(), solve))
 
 def inspect(seconds):
   input('\nInspection time: {} seconds. Press ENTER to start!'.format(seconds))
@@ -36,13 +49,12 @@ def inspect(seconds):
     print(seconds)
     seconds -= 1
 
-sessionTimes = {}
+sessionSolves = []
 
 def main():
   while True:
-    displaySessionTimes(sessionTimes)
+    displaySessionSolves(sessionSolves)
     solveTime = timeSolve()
-    #print('Result: {} seconds.'.format(solveTime))
     saveSolveToSession(solveTime)
 
 if __name__ == '__main__':
