5b38498b60435ef9441859bb1b3bba62198b8cec
Michael Zeevi <michael.52@msn.com>
2019-04-02 09:37:27 +0300

    asdf

--- timer.py
+++ timer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import time
 
-x = input('Press enter to start timer.')
+x = input('Press enter to start timer...')
 startTime = time.time()
 print('Timing...')
-x = input('Press enter to stop timer!')
+x = input('Press enter to stop timer!...')
 print('Stopped.')
 endTime = time.time()
 
 result = round(endTime - startTime, 4)
-print('Timed {} seconds.'.format(result))
+print('Result: Timed {} seconds.'.format(result))
