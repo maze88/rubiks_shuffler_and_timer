92020daa299624d9cec9fa8e922d42b697ce21f9
Michael Zeevi <michael.52@msn.com>
2019-04-02 09:27:53 +0300

    initial commit for timer

---
+++ timer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import time
+
+x = input('Press enter to start timer.')
+startTime = time.time()
+print('Timing...')
+x = input('Press enter to stop timer!')
+print('Stopped.')
+endTime = time.time()
+
+result = round(endTime - startTime, 4)
+print('Timed {} seconds.'.format(result))
