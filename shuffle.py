968d2468a833033c3ca7a4128e33f4491d289eb5
maze88 <michael.52@msn.com>
2019-04-09 14:29:51 +0300

    refactoring started

--- shuffle.py
+++ shuffle.py
@@ -18,31 +18,24 @@ class Move:
       exit()
     # Init turns
     if clockwiseTurns == 'random':
-      self.turns = random.randint(1, 3)
+      self.__turns = random.randint(1, 3)
     elif clockwiseTurns in (1, 2, 3):
-      self.turns = clockwiseTurns
+      self.__turns = clockwiseTurns
     else:
       print('Illegal clockwise turn count chosen for Move object. Please use 1, 2 or 3.')
       exit()
-
-  def face(self):
-    """Returns a single charactar, representing which face of the cube turns in move."""
-    return self.face
+    if __turns == 2:
+      self.turns = 2
+    else:
+      self.turns = 1
 
   def direction(self):
     """Returns an [empty] string with the move's direction.
        Empty is clockwise, prime `'` is counter clockwise."""
-    if clockwiseTurns == 2:
+    if self.__turns == 2:
       return '\''
     else:
       return ''
-
-  def turns(self):
-    """Returns an integer of the amount of quarter turns in move."""
-    if clockwiseTurns == 2:
-      return clockwiseTurns
-    else:
-      return 1
 
   def isClockwise(self):
     """Returns True/False regarding direction of move's turn."""
@@ -61,16 +54,18 @@ class Move:
   def name(self):
     """Returns a string of the move's cube notation, example `L'`."""
     name = ''
-    name += '' # todo
+    name += self.face
+    name += self.direction
     return name
 
 
 def main(n, spacing):
   print('Generating {} random moves...'.format(n))
-  for move in range(n):
-    if move % spacing == 0:
+  for m in range(n):
+    if m % spacing == 0:
       print('')  # add spacing.
-    print(randMove())
+    move = Move()
+    print(move.name())
   print('') # trailing new line.
 
 if __name__ == '__main__':
