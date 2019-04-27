96e7297074c65058e3b7527efd55029819131d00
maze88 <michael.52@msn.com>
2019-04-27 14:28:19 +0300

    added function to recycling similar moves in shuffle script.

--- shuffle.py
+++ shuffle.py
@@ -4,10 +4,19 @@ import random
 import sys
 from moveClass import Move
 
+def recycleIfSimilar(currentMove, previousMove):
+  """If currentMove and previousMove are similar (can cancel out), it generates a new currentMove. Returns currentMove."""
+  if previousMove and currentMove.isSimilarTo(previousMove):
+    previousMove = currentMove
+    currentMove = recycleIfSimilar(Move(), currentMove)
+  return currentMove
+
 def shuffle(moves = 32, spacing = 4):
+  previousMove = 0
   print('\nGenerating {} random moves...'.format(moves))
   for i in range(moves):
-    move = Move()
+    move = recycleIfSimilar(Move(), previousMove)
+    previousMove = move
     print(move.cubeNotation, end = ' ')
     if spacing and (i % spacing) == (spacing - 1):
         print('\t', end = '')
